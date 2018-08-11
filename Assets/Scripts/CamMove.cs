96251f97f482918abe769c51e84a4cb52b2aaf4b
Shubham <40598861+vijit101@users.noreply.github.com>
2018-08-11 18:03:13 +0530

    Bounce-A-Bounce (Commit 1  - Jump Experiment)
    Testing The game with a little jump code with jump upto max y  as max y is equal to floor height which player touches plus 1.3f (a test value for max height in y till which the jump works)
     +Next commit will seek to improve jump

---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+    public GameObject playerreference;
+    Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - playerreference.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerreference.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour {
+    private void OnCollisionStay(Collision collidedfloor)
+    {
+        maxy = collidedfloor.transform.position.y + 1.3f;
+    }
+
+    Rigidbody rgbd;
+    [SerializeField] float speed = 5;
+    [SerializeField] float jumpfactor = 5;
+    [SerializeField] float maxy;
+    private void Awake()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        jumpball();  
+	}
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rgbd.AddForce(new Vector3(horizontal, 0, vertical)*speed);
+    }
+    private void jumpball()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+            if (transform.position.y < maxy)
+            {
+                rgbd.AddForce((Vector3.up)*jumpfactor,ForceMode.Impulse);
+               
+            }
+       }
+        
+    }
+}
